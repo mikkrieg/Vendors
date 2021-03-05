46b60dc3e59837f1cd6097fa355d9b673ce2c820
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-05 13:51:40 -0800

    Create routes for vendor index, can now add vendors to index in list

--- Pierre.Solutions/Pierre/Controllers/VendorsController.cs
+++ Pierre.Solutions/Pierre/Controllers/VendorsController.cs
@@ -4,8 +4,26 @@ using System.Collections.Generic;
 
 namespace Pierre.Controllers
 {
-    public class VendorController : Controller
+    public class VendorsController : Controller
     {
-        
+        [HttpGet("/vendors")]
+        public ActionResult Index()
+        {
+            List<Vendor> allVendors = Vendor.GetAll();
+            return View(allVendors);
+        }
+
+        [HttpGet("/vendors/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost("/vendors")]
+        public ActionResult Create(string vendorName, string vendorDescription)
+        {
+            Vendor newVendor = new Vendor(vendorName, vendorDescription);
+            return RedirectToAction("Index");
+        }
     }
 }
