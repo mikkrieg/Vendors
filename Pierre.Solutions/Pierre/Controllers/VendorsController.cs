210fc819b51f3aa1800002ccbd6e57cb6b76f23a
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-05 12:04:00 -0800

    WIP: Implementing Vendor Controller

---
+++ Pierre.Solutions/Pierre/Controllers/VendorsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Pierre.Models;
+using System.Collections.Generic;
+
+namespace Pierre.Controllers
+{
+    public class VendorController : Controller
+    {
+        
+    }
+}
